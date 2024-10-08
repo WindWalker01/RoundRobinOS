1f10219d92eb58f8a194c3e747be6874f81a95b2
Kyle <kmccsabado23@bsu.edu.ph>
2024-10-08 12:36:12 +0800

    Add Process and Storage class

--- src/Window.java
+++ src/Window.java
@@ -1,11 +1,13 @@
 import javax.swing.*;
 
 public  class Window extends JFrame {
+        JFrame frame;
+        JLabel label;
 
     public Window(int width, int height) {
 
 
-        setSize(width, height);
+        frame.setSize(500, 800);
         setTitle("Round Robin");
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setVisible(true);
